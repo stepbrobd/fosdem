bf7fb73447447d540d655a38c1df72635004a646
Yifei Sun <ysun@hey.com>
2026-01-26 13:43:18 +0100

    get rid of bpf interp

--- test/kernel.nix
+++ test/kernel.nix
@@ -6,6 +6,11 @@
   boot.kernelPackages = pkgs.linuxPackagesFor (
     pkgs.linuxKernel.kernels.linux_latest.override {
       structuredExtraConfig = with lib.kernel; {
+        BPF = lib.mkForce yes;
+        BPF_SYSCALL = lib.mkForce yes;
+        BPF_JIT = lib.mkForce yes;
+        BPF_JIT_ALWAYS_ON = lib.mkForce yes;
+        BPF_JIT_DEFAULT_ON = lib.mkForce yes;
         BPF_KPROBE_OVERRIDE = lib.mkForce yes;
         FUNCTION_ERROR_INJECTION = lib.mkForce yes;
       };
